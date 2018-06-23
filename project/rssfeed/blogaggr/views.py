0cf16eb03f9d702b3ca19f57ceaf73d99d26c5cf
Raghavareddy21/RSS-feed <challavenkataraghavareddy21@gmail.com>
2018-06-24 00:17:02 +0530

    rss feed project

---
+++ project/rssfeed/blogaggr/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from . import forms
+from . import models
+from django.http import HttpResponse
+import re
+import feedparser
+
+# Create your views here.
+
+def feed(request):
+	return render(request, 'blogaggr/home.html')
+
+def search(request):
+    if 'q' in request.GET:
+    	try:
+    		d = feedparser.parse(request.GET['q'])
+    		s =  d['feed']['title'] + '.' + '\n' + d['feed']['description'] + '\n'
+    		for post in d.entries:
+        		s = s + '\n' + post.title + '\n'
+        		sm = post.summary
+        		sm = re.sub('<.*?>','',sm)
+        		s = s + '\n' + sm + '\n'
+    		for post in d.entries:
+    			s = s + post.link
+    		models.link(url = request.GET['q']).save()
+    		return HttpResponse(s)
+    	except KeyError:
+    		return HttpResponse("You searched for an invalid rss feed")
+    else:
+        return HttpResponse("You submitted an empty input")
